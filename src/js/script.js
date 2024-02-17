610c8e719571517cb8cf11ef0f745c9488bde581
dhrin1 <alhdrin97@gmail.com>
2024-02-17 09:54:11 +0800

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+import * as THREE from "three";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  95,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+console.log(window.innerWidth / window.innerHeight);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate(time) {
+  box.rotation.x = time / 1000;
+  box.rotation.y = time / 1000;
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
